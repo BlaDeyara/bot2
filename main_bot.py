2e11122081fc255d6fc88774de43b7eb28721bc7
BlaDeyara <bazhenovma13@gmail.com>
2018-08-02 23:31:46 +0400

    test4

--- main_bot.py
+++ main_bot.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import telebot
 import ini
 from telebot import types
@@ -10,12 +11,15 @@ KeyBuy.row('/Buy')
 @bot.message_handler(Commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,'Привет! Купи слона!',reply_markup=KeyBuy)
-@bot.message_handler(content_types='text')
-def RepeatAll(message):
-    Slon = 'Все говорят: "' + message.text + '", а ты купи слона!'
-    bot.send_message(message.chat.id,Slon,reply_markup=KeyBuy)
+
 @bot.message_handler(commands=['Buy'])
 def Buy(message):
     bot.send_message(message.chat.id,'Кинь денег на карту сбер по номеру телефона +79828325600.')
+
+@bot.message_handler(content_types=["text"])
+def RepeatAll(message):
+    Slon = 'Все говорят: "' + message.text + '", а ты купи слона!'
+    bot.send_message(message.chat.id,Slon,reply_markup=KeyBuy)
+
 
 bot.polling(none_stop=True)
