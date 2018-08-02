175a0783664c8a9fdae758d5ec17db7474a87f81
BlaDeyara <bazhenovma13@gmail.com>
2018-08-02 23:14:42 +0400

    test3

--- main_bot.py
+++ main_bot.py
@@ -3,19 +3,19 @@ import ini
 from telebot import types
 
 bot = telebot.TeleBot(ini.TOKEN)
-"""
-ip = '195.201.137.246'
-port = '1080'
 
-apihelper.proxy = {
-  'https': 'socks5://195.201.137.246:1080'
-}
-"""
 KeyBuy = types.ReplyKeyboardMarkup(True,False)
 KeyBuy.row('/Buy')
 
 @bot.message_handler(Commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,'Привет! Купи слона!',reply_markup=KeyBuy)
+@bot.message_handler(content_types='text')
+def RepeatAll(message):
+    Slon = 'Все говорят: "' + message.text + '", а ты купи слона!'
+    bot.send_message(message.chat.id,Slon,reply_markup=KeyBuy)
+@bot.message_handler(commands=['Buy'])
+def Buy(message):
+    bot.send_message(message.chat.id,'Кинь денег на карту сбер по номеру телефона +79828325600.')
 
 bot.polling(none_stop=True)
