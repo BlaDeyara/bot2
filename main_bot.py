3821d5e18f0c999da5aeac652eff25c9ba9b9136
BlaDeyara <42010282+BlaDeyara@users.noreply.github.com>
2018-08-03 08:23:25 +0400

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 import telebot
 import ini
+import os
 from telebot import types
+from flask import Flask, request
+import logging
 
 bot = telebot.TeleBot(ini.TOKEN)
+server = Flask(__name__)
 
 KeyBuy = types.ReplyKeyboardMarkup(True,False)
 KeyBuy.row('/Buy')
@@ -21,5 +25,19 @@ def RepeatAll(message):
     Slon = 'Все говорят: "' + message.text + '", а ты купи слона!'
     bot.send_message(message.chat.id,Slon,reply_markup=KeyBuy)
 
+@server.route('/' + ini.TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
-bot.polling(none_stop=True)
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://morning-spire-74999.herokuapp.com/' + ini.TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+#bot.polling(none_stop=True)
